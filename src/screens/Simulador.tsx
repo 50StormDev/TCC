7fc8fbcce32d2b3550c27e982b0b1f608db409bd
personal <50StormDev@gamil.com>
2023-10-04 23:55:36 +0900

    bug fix

--- src/screens/Simulador.tsx
+++ src/screens/Simulador.tsx
@@ -3,7 +3,7 @@ import Center from "../components/utils/Center";
 export default function Simulador(){
   return(
     <Center>
-      <h1>Pagina do Simulador</h1>
+      <h1>Simulador</h1>
     </Center>
   )
 }
