e944326f00ef08582e6ba9802a2a847139f75a0f
personal <50StormDev@gamil.com>
2023-10-05 21:31:02 +0900

    Change project struct and start developing dashboard screen

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,9 +1,12 @@
 import Center from "../../components/utils/Center";
+import DashboardCalendar from "./DashboardCalendar";
 
 export default function Dashboard() {
   return (
     <Center>
       <h1>DashBoard</h1>
+      <h3>Total Aproximado 300.000¥</h3>
+      <DashboardCalendar/>
     </Center>
 
   )
